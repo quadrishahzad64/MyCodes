a833cbbf1dc97b08192188db247dcb8d89f0875d
quadrishahzad64 <quadrishahzad64@gmail.com>
2024-12-19 21:23:49 +0500

    MyCode

--- ListToUpperCase.dart
+++ ListToUpperCase.dart
@@ -2,8 +2,7 @@ void main() {
   List<String> fruits = ['shahzad', 'arif', 'saleem', 'bilal'];
   for (var i = 0; i < fruits.length; i++) {
     fruits[i] = fruits[i].toUpperCase();
-    {
-    }
+    {}
   }
-      print(fruits);
+  print(fruits);
 }
