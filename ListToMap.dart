216f41c2c413e230e3a3b7858a4d0f32428eec70
quadrishahzad64 <quadrishahzad64@gmail.com>
2024-12-20 23:36:03 +0500

    MyCodes

--- ListToMap.dart
+++ ListToMap.dart
@@ -1,18 +1,8 @@
 void main() {
-
-  List mList = [1, 3, 5, 7, 9];
-  List mList1 = [2, 4, 6, 8, 10];
-  List mList2 = [];
-
-  int i = 0;
-  int j = 0;
-
-  while (i < mList.length)  {
-    mList2.add(mList[i]);
-    i++;
-    mList2.add(mList1[j]);
-    j++;
+  List myList = ["Shahzad", "Kamran", "Arif", "Bilal", "Salim"];
+  Map myMap = {};
+  for (var i = 0; i < myList.length; i++) {
+    myMap[i] = myList[i];
   }
-  print(mList2);
+  print(myMap);
 }
-
