0558790ea001c952b8baffb08e4a801a3fc7bc85
quadrishahzad64 <quadrishahzad64@gmail.com>
2024-12-20 21:09:03 +0500

    Update ListToUpperCase.dart

--- ListToUpperCase.dart
+++ ListToUpperCase.dart
@@ -1,6 +1,5 @@
 void main() {
-  var vals2 = [-2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  vals2.removeWhere((a) => a <= 0);
-  vals2 = vals2.toSet().toList();
+  List<String> text = ["salim", "shahzad", "imran", "salman"];
+  var vals2 = text.map((e) => e.toUpperCase()).toList();
   print(vals2);
 }
