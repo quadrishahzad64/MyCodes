5d135f4cf04c84ad3ae95f37443bc73f577f2291
quadrishahzad64 <quadrishahzad64@gmail.com>
2024-12-20 23:39:37 +0500

    MyCodes

--- ListToMap.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  List myList = ["Shahzad", "Kamran", "Arif", "Bilal", "Salim"];
-  Map myMap = {};
-  for (var i = 0; i < myList.length; i++) {
-    myMap[i] = myList[i];
-  }
-  print(myMap);
-}
