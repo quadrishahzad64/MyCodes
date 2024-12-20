f5757f40c082b41e9c53445df9dd40e1ec49d631
quadrishahzad64 <quadrishahzad64@gmail.com>
2024-12-20 23:54:52 +0500

    MyCodes

--- MergeListSorted.dart
+++ MergeListSorted.dart
@@ -1,7 +1,16 @@
 void main() {
-  List<String> Names = ['shahzad', 'arif', 'saleem', 'bilal'];
-  for (var i = 0; i < Names.length; i++) {
-    Names[i] = Names[i].toUpperCase();
+  List mList = [1, 3, 5, 7, 9];
+  List mList1 = [2, 4, 6, 8, 10];
+  List mList2 = [];
+
+  int i = 0;
+  int j = 0;
+
+  while (i < mList.length) {
+    mList2.add(mList[i]);
+    i++;
+    mList2.add(mList1[j]);
+    j++;
   }
-  print(Names);
+  print(mList2);
 }
